b9d1eb28a51e031f8ec636a4e4afd8ae2781e06d
mohitmv <mohitsaini1196@gmail.com>
2015-10-19 22:15:07 +0530

    gettting lan lng

--- mslib/alldef/kurry/form.cpp
+++ mslib/alldef/kurry/form.cpp
@@ -4,7 +4,7 @@ define bigsearch() {
 			p("&nbsp;");
 		}
 		div(class: "col m8 s12 l9", style:{"padding": "0px", "margin": "0px"}) {
-			input(attr:{placeholder: ph, id: id}, class:"bigsearch definput", style:{"border-radius": "0px"});
+			input(attr:{placeholder: ph, id: id, autofocus: autofocus}, class:"bigsearch definput", style:{"border-radius": "0px"});
 		}
 		div(class: "col m2 s12 l1 ", style:{"padding": "0px", "margin": "0px"}) {
 			button(class: "bigsearchbutton waves-effect waves-light btn", style:{"border-radius": "0px"}) {
--- templates/index.cpp
+++ templates/index.cpp
@@ -1,11 +1,12 @@
 main(js:["js/index.js", "https://maps.googleapis.com/maps/api/js?signed_in=true&libraries=drawing,places&callback=initMap"], bodystyle:{"background-image": 'url("photo/slide1.jpg")', "background-size": "auto 100%", "background-position": "center" }) {
 	header2(tablink:[HOST, "", "", "", "", "", ""], tabname:["Home", "Our Story", "Blog", "Be a Chef", "Contact Us", "Login"]);
+	div(attr:{id: "googlemap"});
 	div() {
 		div(attr:{align: "center"}) {
 			div(class: "pagecenter container") {
 				bigf(name: "Super Hit Meal From Super Hit Chefs", color: "white");
 				height(val:40);
-				bigsearch(ph:"Enter Your Location", id:"locsearch");
+				bigsearch(ph:"Enter Your Location", id:"locsearch", autofocus:"");
 			}
 		}
 	}
